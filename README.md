36d3403050e4b0aa9be9e6b0743723eb9fabb794
kjorgecaballero <kja.cruzcaballero@gmail.com>
2025-11-29 02:50:50 -0600

    Change URL and user

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### MEDIEVIL BLENDER TOOLS
 
 Medievil tools for psx modding
+Big test
--- addon_updater_ops.py
+++ addon_updater_ops.py
@@ -1357,7 +1357,7 @@ def register(bl_info):
     # Choose your own repository, must match git name for GitHUb and Bitbucket,
     # for GitLab use project ID (numbers only).
     updater.repo = "blender-medievil"
-
+    
     # updater.addon = # define at top of module, MUST be done first
 
     # Website for manual addon download, optional but recommended to set.
